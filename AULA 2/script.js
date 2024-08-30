afe75adb8a2613e066a8d4c856f7788594061f1b
Luis <luisfelipechristogomes@gmail.com>
2024-08-29 21:32:42 -0300

    Add files via upload

---
+++ AULA 2/script.js
@@ -0,0 +1,6 @@
+console.log("Olá mundo")
+let preço = prompt("Digite um preço");
+document.write(preço, ". ");
+
+let nome = prompt("Digite seu nome");
+document.write("Olá, ",  nome, ", seja bem vindo ao site");
