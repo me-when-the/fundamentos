c6701cc15c54700d77d1f99e1a806f89166fc287
Luis <luisfelipechristogomes@gmail.com>
2024-09-06 18:40:25 -0300

    Add files via upload

--- Tipagem/caixaeletronico.js
+++ Tipagem/caixaeletronico.js
@@ -3,9 +3,9 @@ valorDoSaque = prompt('informe o valor do saque: ')
 
 if(valorDoSaque <= saldoDisponivel){
     saldoDisponivel = saldoDisponivel - valorDoSaque
-document.write("Voce esta sacando R$ ", valorDoSaque, ".")
+document.write("Voce esta sacando R$ ", valorDoSaque, ". ")
 } else{
     document.write("O valor solicitado e maior que o valor disponivel para saque!")
 }
 
-    document.write("Saldo disponivek R$ ", saldoDisponivel)
+    document.write("Saldo disponivel R$ ", saldoDisponivel)
